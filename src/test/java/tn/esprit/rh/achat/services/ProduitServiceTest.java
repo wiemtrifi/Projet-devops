75335e23fccef849a9a38023967fce8d7c1dce2d
ikramneji <ikram.neji@esprit.tn>
2023-11-08 14:58:29 +0100

    test

---
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
@@ -0,0 +1,22 @@
+package tn.esprit.rh.achat.services;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.rh.achat.entities.Produit;
+
+import java.util.List;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ProduitServiceTest {
+    @Autowired
+    IProduitService ps;
+    @Test
+    @Order(1)
+    public void testRetreiveAllUsers(){
+        List<Produit> listProduits=ps.retrieveAllProduits();
+        Assertions.assertEquals(0,listProduits.size());
+    }
+
+}
